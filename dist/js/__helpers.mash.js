bab2a8c043721da9b193fa0079923f0a73e7f5d0
cferdinandi <chris@gomakethings.com>
2017-07-30 14:54:17 -0400

    Added m$, a helper library

---
+++ dist/js/__helpers.mash.js
@@ -0,0 +1,173 @@
+/*!
+ * blackbeard v0.2.0: Future portal layout
+ * (c) 2017 Chris Ferdinandi
+ * BSD-3-Clause License
+ * http://github.com/mashery/blackbeard
+ */
+
+var m$ = (function () {
+
+	'use strict';
+
+	//
+	// Variables
+	//
+
+	var m$ = {}; // Placeholder for public methods
+
+
+	//
+	// Polyfills
+	//
+
+	// NodeList.prototype.forEach()
+	if (window.NodeList && !NodeList.prototype.forEach) {
+		NodeList.prototype.forEach = function (callback, thisArg) {
+			thisArg = thisArg || window;
+			for (var i = 0; i < this.length; i++) {
+				callback.call(thisArg, this[i], i, this);
+			}
+		};
+	}
+
+	// Object.prototype.forEach()
+	if (!Object.prototype.forEach) {
+		Object.defineProperties(Object.prototype, {
+			'forEach': {
+				value: function (callback) {
+					if (this == null) {
+						throw new TypeError('Not an object');
+					}
+					var obj = this;
+					for (var key in obj) {
+						if (obj.hasOwnProperty(key)) {
+							callback.call(obj, obj[key], key, obj);
+						}
+					}
+				},
+				writable: true
+			}
+		});
+	}
+
+
+	//
+	// Methods
+	//
+
+	m$.get = function (selector, scope) {
+		scope = scope || document;
+		return scope.querySelector(selector) || document.createElement('_');
+	};
+
+	m$.getAll = function (selector, scope) {
+		scope = scope || document;
+		return Array.prototype.slice.call(scope.querySelectorAll(selector));
+	}
+
+	/**
+	 * Simulate a click event.
+	 * @public
+	 * @param {Element} elem  the element to simulate a click on
+	 */
+	m$.click = function (elem) {
+		// Create our event (with options)
+		var evt = new MouseEvent('click', {
+			bubbles: true,
+			cancelable: true,
+			view: window
+		});
+		// If cancelled, don't dispatch our event
+		var canceled = !elem.dispatchEvent(evt);
+	};
+
+	m$.on = function (event, elem, callback, capture) {
+		if (typeof (elem) === 'function') {
+			capture = callback;
+			callback = elem;
+			elem = window;
+		}
+		capture = capture ? true : false;
+		elem.addEventListener(event, callback, capture);
+	};
+
+	m$.off = function (event, elem, callback, capture) {
+		if (typeof (elem) === 'function') {
+			capture = callback;
+			callback = elem;
+			elem = window;
+		}
+		capture = capture ? true : false;
+		elem.removeEventListener(event, callback, capture);
+	};
+
+	m$.addQueryString = function (url, key, value) {
+		return (/[\?]/.test(url) ? '&' : '?') + key + '=' + value;
+	};
+
+	/**
+	 * Get the value of a query string from a URL
+	 * @param  {String} field The field to get the value of
+	 * @param  {String} url   The URL to get the value from [optional]
+	 * @return {String}       The value
+	 */
+	m$.getQueryString = function (field, url) {
+		var href = url ? url : window.location.href;
+		var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
+		var string = reg.exec(href);
+		return string ? string[1] : null;
+	};
+
+	/**
+	 * Merge two or more objects together.
+	 * @param   {Boolean}  deep     If true, do a deep (or recursive) merge [optional]
+	 * @param   {Object}   objects  The objects to merge together
+	 * @returns {Object}            Merged values of defaults and options
+	 * @todo optimize loops
+	 */
+	m$.extend = function () {
+
+		// Variables
+		var extended = {};
+		var deep = false;
+		var i = 0;
+		var length = arguments.length;
+
+		// Check if a deep merge
+		if (Object.prototype.toString.call(arguments[0]) === '[object Boolean]') {
+			deep = arguments[0];
+			i++;
+		}
+
+		// Merge the object into the extended object
+		var merge = function (obj) {
+			for (var prop in obj) {
+				if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+					// If deep merge and property is an object, merge properties
+					if (deep && Object.prototype.toString.call(obj[prop]) === '[object Object]') {
+						extended[prop] = extend(true, extended[prop], obj[prop]);
+					} else {
+						extended[prop] = obj[prop];
+					}
+				}
+			}
+		};
+
+		// Loop through each object and conduct a merge
+		for (; i < length; i++) {
+			var obj = arguments[i];
+			merge(obj);
+		}
+
+		return extended;
+
+	};
+
+
+	//
+	// Public APIs
+	//
+
+	return m$;
+
+})();
